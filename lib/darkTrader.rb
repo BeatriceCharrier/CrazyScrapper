8fc3766cc9e33e9e478bf152a5a1a1f3e04a838b
adrien66 <adrien66.pub@gmail.com>
2019-07-18 17:24:47 +0200

    je pauffine

--- lib/darkTrader.rb
+++ lib/darkTrader.rb
@@ -3,6 +3,8 @@ require 'nokogiri'
 require 'open-uri'
 PAGE_URL = "https://coinmarketcap.com/all/views/all/"
 
+
+#Version Badass !
 page = Nokogiri::HTML(open(PAGE_URL))   
 
 def name_patch(page, i)
@@ -18,35 +20,60 @@ end
 def list_crypto(page)
     list_crypto = Array.new
     
-    50.times do |i|
+
+    #page.xpath('//*[@id="currencies-all"]/tbody/tr').length.
+    #A la place de 10.times mais c'est très long
+    10.times do |i|
         list_crypto << {"#{name_patch(page, i)}": "#{price_patch(page, i)}"}
     end
     return list_crypto
 end 
 
-puts list_crypto(page)
-
-#page.xpath('//*[@id="currencies-all"]/tbody/tr').length.
-#A la place de 50.times
 
 
+#Version simplifié mais pas en cohérence avec l'énoncer
+def second_crypto(page)
+    excel = Hash.new
+    name_crypto = Array.new
+    price_crypto = Array.new
 
-=begin 
-excel = Hash.new
-name_crypto = Array.new
-price_crypto = Array.new
+    page.xpath('//*[@class="link-secondary"]').each do |node|
+        name_crypto << node.text
+    end
+    page.xpath('//*[@class="price"]').each do |node|
+        price_crypto << node.text
+    end
 
-page.xpath('//*[@class="link-secondary"]').each do |node|
-    name_crypto << node.text
+    excel = name_crypto.zip(price_crypto)
+
+    excel.each do |key, value|
+        puts "#{key} : #{value}"
+    end 
 end
-page.xpath('//*[@class="price"]').each do |node|
-    price_crypto << node.text
+
+
+def perform(page)
+    puts "Salut, alors j'ai 2 versions pour toi."
+    puts "1: Version simple pour les noobs"
+    puts "2: Version Badass !"
+    input = 0
+    
+    while input != 1 && input != 2
+        puts "Tape 1 ou 2 :)"
+        input = gets.chomp.to_i
+        
+        if input == 1
+            puts list_crypto(page)
+        elsif input == 2
+            second_crypto(page)
+        else
+            puts "J'ai dit ! 1 ou 2 !!!"
+        end
+    end
 end
 
-excel = name_crypto.zip(price_crypto)
+puts perform(page)
 
-excel.each do |key, value|
-    puts "#{key} : #{value}"
-end 
-=end
+
+
 
